2b0b873db3322a99cc78626c3b658c9648ee2c19
FishingHacks <fishinghacks@proton.me>
2025-04-02 02:32:39 +0200

    set config, added build function and errors

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,46 @@
-use std::path::PathBuf;
-
-#[derive(Debug, Default)]
-pub enum ContentKind {
-    #[default]
-    Markdown,
-}
+use serde::{Deserialize, Serialize};
+use std::path::{Path, PathBuf};
 
 #[derive(Debug)]
 pub struct SiteConfig {
-    resources_dir: PathBuf,
+    resources_dirs: Vec<PathBuf>,
     templates_dir: PathBuf,
     content_dir: PathBuf,
+    out_dir: PathBuf,
+    index_page: String,
+}
+
+#[derive(Default, Debug, Serialize, Deserialize)]
+pub struct UnresolvedSiteConfig {
+    resources_dirs: Option<Vec<PathBuf>>,
+    templates_dir: Option<PathBuf>,
+    content_dir: Option<PathBuf>,
+    out_dir: Option<PathBuf>,
+    index_page: Option<String>,
+}
+
+impl SiteConfig {
+    pub fn from_unresolved(root_dir: &Path, unresolved: UnresolvedSiteConfig) -> Self {
+        let resources_dirs = match unresolved.resources_dirs {
+            Some(dirs) => dirs.into_iter().map(|v| root_dir.join(v)).collect(),
+            None => vec![root_dir.join("resources")],
+        };
+        let templates_dir = resolve_path(root_dir, unresolved.templates_dir, Path::new("templates"));
+        let content_dir = resolve_path(root_dir, unresolved.content_dir, Path::new("content"));
+        let out_dir = resolve_path(root_dir, unresolved.out_dir, Path::new("dist"));
+        let index_page = unresolved.index_page.unwrap_or_else(|| "_index.md".into());
+        Self {
+            resources_dirs,
+            templates_dir,
+            content_dir,
+            out_dir,
+            index_page,
+        }
+    }
+}
+
+fn resolve_path(root_dir: &Path, path: Option<PathBuf>, default: &'static Path) -> PathBuf {
+    let Some(path) = path else { return root_dir.join(default) };
+    // this sucks, but i would like to have absolute paths
+    root_dir.join(path)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod config;
 mod content;
 
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use clap::{Parser, Subcommand};
+use config::{SiteConfig, UnresolvedSiteConfig};
+use thiserror::Error;
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
@@ -30,20 +32,51 @@ fn main() {
     let args = Args::parse();
 
     match args.command {
-        Commands::Serve { .. } => {
-            // read config file
-            // index resources
-            // index templates
-            // parse content
-            // build website (copy files and stuff)
+        Commands::Serve { path, port } => {
+            let path = path
+                .map(Ok)
+                .unwrap_or_else(std::env::current_dir)
+                .expect("failed to get the current directory");
+            let port = port.unwrap_or(8000);
+            let config = build(&path).expect("failed to build");
             // serve (open up a server)
         }
-        Commands::Build { .. } => {
-            // read config file
-            // index resources
-            // index templates
-            // parse content
-            // build website (copy files and stuff)
-        }
+        Commands::Build { path: Some(path) } => _ = build(&path).expect("failed to build"),
+        Commands::Build { path: None } => _ = build(&std::env::current_dir().unwrap()).expect("failed to build"),
     }
+}
+
+#[derive(Debug, Error)]
+enum BuildError {
+    #[error("{0}")]
+    Config(#[from] ConfigReadError),
+}
+
+#[derive(Debug, Error)]
+enum ConfigReadError {
+    #[error("{0}")]
+    Toml(#[from] toml::de::Error),
+    #[error("{0}")]
+    IO(#[from] std::io::Error),
+}
+
+fn read_config(root_dir: &Path) -> Result<SiteConfig, ConfigReadError> {
+    let config_path = root_dir.join("config.toml");
+    if !config_path.exists() {
+        return Ok(SiteConfig::from_unresolved(root_dir, Default::default()));
+    }
+    let config_str = std::fs::read_to_string(&config_path)?;
+    let unresolved_config = toml::from_str::<UnresolvedSiteConfig>(&config_str)?;
+    Ok(SiteConfig::from_unresolved(root_dir, unresolved_config))
+}
+
+fn build(root_dir: &Path) -> Result<SiteConfig, BuildError> {
+    // read config file
+    let config = read_config(root_dir)?;
+
+    // index resources
+    // index templates
+    // parse content
+    // build website (copy files and stuff)
+    todo!()
 }
